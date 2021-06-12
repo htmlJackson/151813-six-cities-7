65c9635f7ac9eeab59a9237d26d2be1788243b80
Evgeny Ber <fiend.jackson@gmail.com>
2021-06-12 21:46:21 +0300

    настроил предварительный роутер

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Main from '../main/main';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+
+import {AppRoute} from '../../const.js';
+import Main from '../pages/main/main';
+import SignIn from '../pages/signIn/signIn';
+import Favorites from '../pages/favorites/favorites';
+import Room from '../pages/room/room';
+import NotFound from '../pages/notFound/notFound';
 
 function App(props) {
   const {cardsCounter} = props;
+
   return (
-    <Main cardsCounter={cardsCounter} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <Main cardsCounter={cardsCounter} />
+        </Route>
+        <Route exact path={AppRoute.SIGN_IN}>
+          <SignIn />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <Room />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components/pages/notFound/notFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFound(props) {
+  return (
+    <div className="page page--gray page--main">
+      <h1>Page not found</h1>
+    </div>
+  );
+}
+
+
+export default NotFound;
