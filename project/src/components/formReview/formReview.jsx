68e4cb03f3dde82ff3f32c6501afaf4758009cec
Evgeny Ber <fiend.jackson@gmail.com>
2021-06-23 18:07:46 +0300

    убрал лишние проверки с радио

--- project/src/components/formReview/formReview.jsx
+++ project/src/components/formReview/formReview.jsx
@@ -28,31 +28,31 @@ function FormReview() {
     >
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" id="5-stars" type="radio" value={5} onChange={handleChange} checked={formReviewData.rating === 5}/>
+        <input className="form__rating-input visually-hidden" name="rating" id="5-stars" type="radio" value={5} onChange={handleChange} />
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width={37} height={33}>
             <use xlinkHref="#icon-star" />
           </svg>
         </label>
-        <input className="form__rating-input visually-hidden" name="rating" id="4-stars" type="radio" value={4} onChange={handleChange} checked={formReviewData.rating === 4} />
+        <input className="form__rating-input visually-hidden" name="rating" id="4-stars" type="radio" value={4} onChange={handleChange} />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width={37} height={33}>
             <use xlinkHref="#icon-star" />
           </svg>
         </label>
-        <input className="form__rating-input visually-hidden" name="rating" id="3-stars" type="radio" value={3} onChange={handleChange} checked={formReviewData.rating === 3} />
+        <input className="form__rating-input visually-hidden" name="rating" id="3-stars" type="radio" value={3} onChange={handleChange} />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width={37} height={33}>
             <use xlinkHref="#icon-star" />
           </svg>
         </label>
-        <input className="form__rating-input visually-hidden" name="rating" id="2-stars" type="radio" value={2} onChange={handleChange} checked={formReviewData.rating === 2} />
+        <input className="form__rating-input visually-hidden" name="rating" id="2-stars" type="radio" value={2} onChange={handleChange} />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width={37} height={33}>
             <use xlinkHref="#icon-star" />
           </svg>
         </label>
-        <input className="form__rating-input visually-hidden" name="rating" id="1-star" type="radio" value={1} onChange={handleChange} checked={formReviewData.rating === 1} />
+        <input className="form__rating-input visually-hidden" name="rating" id="1-star" type="radio" value={1} onChange={handleChange} />
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width={37} height={33}>
             <use xlinkHref="#icon-star" />
